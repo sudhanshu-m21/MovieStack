3ac890cb90ccc47e879ef403a23fd91014ed5b93
sudhanshu-m21 <sudhanshumishra106@gmail.com>
2025-05-19 17:54:13 +0530

    changed logo

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
 export const LOGO =
-  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+  "https://static.vecteezy.com/system/resources/thumbnails/008/350/760/small_2x/initial-m-logo-design-vector.jpg";
+// export const LOGO =
+//   "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
 export const API_OPTIONS = {
   method: "GET",
